f7076dc43dba29035ed865702205bfba82489cfd
Claire Sersun <claire.sersun@gmail.com>
2023-03-05 17:07:10 -0500

    update files

--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,5 +1,23 @@
 // TODO: import actions and implement reducer for each action
-export default function reducer() {}
+import {
+  ADD_BOOK,
+  REMOVE_BOOK,
+  SEARCH_BOOKS 
+} from '/actions.js'
+
+export default function reducer(prevState, {action, payload}) {
+  const {books} = prevState
+  switch(action) {
+    case ADD_BOOK:
+      return {...prevState, books: saveToLocalStorage(books.push(payload))} 
+    case REMOVE_BOOK:
+      return {...prevState, books: saveToLocalStorage(books.filter(book => book.id != payload.id))} 
+    case SEARCH_BOOKS:
+      return {...prevState}
+    default:
+      return prevState
+  }
+}
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
--- src/context/book/state.js
+++ src/context/book/state.js
@@ -1,8 +1,17 @@
-// TODO: Load/parse 'favoriteBooks' from localStorage
-// TODO: Export initial state object as default
-// initial state should have keys bookSearchResults and favoriteBooks
-// bookSearchResults should be an empty array
-// favoriteBooks should be the value from localStorage or an empty array if localStorage value is falsy
-const initialState = {}
+// TODO: Load/parse 'favoriteBooks' from localStorage √
+// TODO: Export initial state object as default √
+// initial state should have keys bookSearchResults and favoriteBooks √
+// bookSearchResults should be an empty array √
+// favoriteBooks should be the value from localStorage or an empty array if localStorage value is falsy √
+
+function books() {
+    if (!JSON.parse(localStorage.getItem('favoriteBooks'))) return []
+    else return JSON.parse(localStorage.getItem('favoriteBooks'))
+}
+
+const initialState = {
+    bookSearchResults: [], 
+    favoriteBooks: books
+}
 
 export default initialState
